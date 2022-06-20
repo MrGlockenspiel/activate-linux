2bad35efbe2e8246165a018f18921d1be2775a27
Tamara Schmitz <tamara.schmitz@suse.com>
2022-06-20 10:05:08 +0200

    have build-rpm.sh consider version nr in spec file

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
+set -xeou
 
 ###
 # Run it on fedora or else
 ###
 
-# Create rpmbuild structure
 rpm_dir="$HOME/rpmbuild"
+tar_dir="/tmp"
+cur_dir=$(pwd)
+
+tar_suffix=".tar.gz"
+tar_name=activate-linux-$(sed -nr 's/^Version: (.+$)/\1/p' activate-linux.spec)
+tar_file=$tar_name$tar_suffix
+
+# Create rpmbuild structure
 mkdir -p "$rpm_dir/RPMS"
 mkdir -p "$rpm_dir/SPECS"
 mkdir -p "$rpm_dir/SOURCES"
 
 # Build tarfile
-tar_out="/tmp/activate-linux-git.tar.gz"
-cur_dir=$(pwd)
-mkdir -p /tmp/tarbuild/
-cp -r . /tmp/tarbuild/activate-linux-git
-cd /tmp/tarbuild || exit
-tar -czf $tar_out .
+rm -rf "$tar_dir/tarbuild/"
+mkdir -p "$tar_dir/tarbuild/"
+cp -r . "$tar_dir/tarbuild/$tar_name"
+cd "$tar_dir/tarbuild" || exit
+tar -czf $tar_dir/$tar_file .
 cd "$cur_dir" || exit
 
 # Build RPM
-cp $tar_out "$rpm_dir/SOURCES/"
-cp activate-linux-rpm.spec "$rpm_dir/SPECS/"
+cp $tar_dir/$tar_file "$rpm_dir/SOURCES/$tar_file"
+cp activate-linux.spec "$rpm_dir/SPECS/"
 rpmbuild -bb "$rpm_dir/SPECS/activate-linux.spec" --nodebuginfo
 
 # Clean
-rm -rf /tmp/tarbuild
-rm $tar_out
+rm -rf $tar_dir/tarbuild
+rm "$tar_dir/$tar_file"
