d064851d8c7aa4616d7acc57e636761b7949fa2a
Mubashshir <ahmubashshir@gmail.com>
2022-08-29 01:32:13 +0600

    i18n: Document the helper macros
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- src/i18n-helper.h
+++ src/i18n-helper.h
@@ -16,14 +16,84 @@ typedef struct {
 
 typedef char* lang_map;
 
+/****************************************************************
+ * match_str(match, with)
+ *
+ * Check whether @match matches @with
+ * @note  both @match and @with can be at most 5
+ *
+ * @param match Match this param
+ * @param with  Match with this param
+ * @return true if both arg matches
+ ****************************************************************/
 #define match_str(match, with) strncmp(match, with, 5) == 0
+
+/****************************************************************
+ * length(array)
+ *
+ * Return length of @array
+ *
+ * @param array The array to get length of
+ * @return length of @array
+ ****************************************************************/
 #define length(array) (sizeof(array) / sizeof(array[0]))
+
+/****************************************************************
+ * map_preset(name, description)
+ *
+ * Define a preset
+ *
+ * @param name        Preset name
+ * @param description Preset Description
+ * @return preset struct
+ ****************************************************************/
 #define map_preset(name, description) {name, description},
+
+/****************************************************************
+ * _LANG(id, country)
+ *
+ * Define a language code
+ *
+ * @param id      Language code
+ * @param country Country code
+ * @return "(id)_(country)"
+ ****************************************************************/
 #define _LANG(id, country) id "_" country,
 
-// preset generator macro
+/****************************************************************
+ * untranslated(lang)
+ *
+ * Define untranslated preset
+ *
+ * @param lang    Target language
+ * @return i18n_info{NULL, NULL}
+ ****************************************************************/
 #define untranslated(lang)  {NULL, NULL},
+
+/****************************************************************
+ * system_i18n(lang, prefix, suffix, subtitle)
+ *
+ * Define a preset using SYSTEM_NAME
+ *
+ * @param lang     Target language
+ * @param prefix   Title prefix
+ * @param suffix   Title suffix
+ * @param subtitle Watermark subtitle
+ ****************************************************************/
 #define system_i18n(lang, prefix, suffix, subtitle) {prefix SYSTEM_NAME suffix, subtitle},
+
+/****************************************************************
+ * platform_i18n(name, lang, prefix, suffix, presub, sufsub)
+ *
+ * Define a platform specific preset
+ *
+ * @param name    Target platform
+ * @param lang    Target language
+ * @param prefix  Title prefix
+ * @param suffix  Title suffix
+ * @param presub  Subtitle prefix
+ * @param sufsub  Subtitle suffix
+ ****************************************************************/
 #define platform_i18n(name, lang, prefix, suffix, presub, sufsub) \
 						{prefix name suffix, presub name sufsub},
 
