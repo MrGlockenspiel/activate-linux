5419c1ffc4ca131bb3c3c624454b2f9f09a8009f
Sean "Jesus" Cohmer <93715513+aprunge@users.noreply.github.com>
2022-10-17 12:52:56 -0400

    imade it good :D

---
+++ main.rs
@@ -0,0 +1,235 @@
+#![allow(dead_code, unused_variables)]
+
+fn main() {
+    a(true);
+    c(false);
+    t(false);
+    i(false);
+    v(false);
+    a(false);
+    t(false);
+    e(false);
+    space(false);
+    l(true);
+    i(false);
+    n(false);
+    u(false);
+    x(false);
+    print!("\n");
+}
+
+fn a(caps: bool) {
+    if caps == true {
+        print!("A");
+    } else {
+        print!("a");
+    }
+}
+
+fn b(caps: bool) {
+    if caps == true {
+        print!("B");
+    } else {
+        print!("b");
+    }
+}
+
+fn c(caps: bool) {
+    if caps == true {
+        print!("C");
+    } else {
+        print!("c");
+    }
+}
+
+fn d(caps: bool) {
+    if caps == true {
+        print!("D");
+    } else {
+        print!("d");
+    }
+}
+
+fn e(caps: bool) {
+    if caps == true {
+        print!("E");
+    } else {
+        print!("e");
+    }
+}
+
+fn f(caps: bool) {
+    if caps == true {
+        print!("F");
+    } else {
+        print!("f");
+    }
+}
+
+fn g(caps: bool) {
+    if caps == true {
+        print!("G");
+    } else {
+        print!("g");
+    }
+}
+
+fn h(caps: bool) {
+    if caps == true {
+        print!("H");
+    } else {
+        print!("h");
+    }
+}
+
+fn i(caps: bool) {
+    if caps == true {
+        print!("I");
+    } else {
+        print!("i");
+    }
+}
+
+fn j(caps: bool) {
+    if caps == true {
+        print!("J");
+    } else {
+        print!("j");
+    }
+}
+
+fn k(caps: bool) {
+    if caps == true {
+        print!("K");
+    } else {
+        print!("k");
+    }
+}
+
+fn l(caps: bool) {
+    if caps == true {
+        print!("L");
+    } else {
+        print!("l");
+    }
+}
+
+fn m(caps: bool) {
+    if caps == true {
+        print!("M");
+    } else {
+        print!("m");
+    }
+}
+
+fn n(caps: bool) {
+    if caps == true {
+        print!("N");
+    } else {
+        print!("n");
+    }
+}
+
+fn o(caps: bool) {
+    if caps == true {
+        print!("O");
+    } else {
+        print!("o");
+    }
+}
+
+fn p(caps: bool) {
+    if caps == true {
+        print!("P");
+    } else {
+        print!("p");
+    }
+}
+
+fn q(caps: bool) {
+    if caps == true {
+        print!("Q");
+    } else {
+        print!("q");
+    }
+}
+
+fn r(caps: bool) {
+    if caps == true {
+        print!("R");
+    } else {
+        print!("r");
+    }
+}
+
+fn s(caps: bool) {
+    if caps == true {
+        print!("S");
+    } else {
+        print!("s");
+    }
+}
+
+fn t(caps: bool) {
+    if caps == true {
+        print!("T");
+    } else {
+        print!("t");
+    }
+}
+
+fn u(caps: bool) {
+    if caps == true {
+        print!("U");
+    } else {
+        print!("u");
+    }
+}
+
+fn v(caps: bool) {
+    if caps == true {
+        print!("V");
+    } else {
+        print!("v");
+    }
+}
+
+fn w(caps: bool) {
+    if caps == true {
+        print!("W");
+    } else {
+        print!("w");
+    }
+}
+
+fn x(caps: bool) {
+    if caps == true {
+        print!("X");
+    } else {
+        print!("x");
+    }
+}
+
+fn y(caps: bool) {
+    if caps == true {
+        print!("Y");
+    } else {
+        print!("y");
+    }
+}
+
+fn z(caps: bool) {
+    if caps == true {
+        print!("Z");
+    } else {
+        print!("z");
+    }
+}
+
+fn space(tab: bool) {
+    if tab == true {
+        print!("    ");
+    } else {
+        print!(" ");
+    }
+}
