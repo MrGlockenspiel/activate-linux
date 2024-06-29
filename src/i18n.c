dedab57ce09683d54b64256b03c29ad44df7ece7
thisiselias <elias.fransson086@gmail.com>
2024-06-30 01:12:44 +0200

    sv_SE localization

--- src/i18n.c
+++ src/i18n.c
@@ -63,6 +63,8 @@ i18n_info_soup langs[] = {
     {"無需啟用 ", "", "我們不像 Microsoft 那樣煩人。"}},
   {"hu_HU", {"A(z) ", " aktiválása", "Aktiválja a(z) ", " rendszert a Gépházban."},
     {"Nem kell aktiválni a(z) ", "-t", "Nem vagyunk olyan idegesítőek,\nmint a Microsoft."}}
+  {"sv_SE", {"Aktivera ", "", "Gå till Inställningar för att aktivera ", "."},
+    {"Du behöver inte aktivera ", "", "Vi är inte lika jobbiga som Microsoft."}},
 };
 
 
